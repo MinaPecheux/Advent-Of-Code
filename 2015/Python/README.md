860ef2ccc97969772bd93bbdeb88bc63b44735d7
Mina Pêcheux <mina.pecheux@gmail.com>
2019-12-25 22:17:43 +0100

    feat(2015/Python): added day1 + prepared 2015/Python folder

---
+++ 2015/Python/README.md
@@ -0,0 +1,26 @@
+# [2015] Python
+
+This subfolder contains my code solutions for challenges from the 2015 series, written in Python (3.6.5).
+
+## Preliminary note: files organization
+In these files, I try to always organize the code in the same way:
+
+1. first, some imports if need be (either Python built-in libs or external packages, **even if I will try and stay as "built-in" as possible in my answers**)
+
+2. then, parsing functions to read and extract data from the provided input (that seems to always be a string of characters)
+
+3. then, the actual computation functions, separated in 3 bits:
+  - util and common functions
+  - functions for the Part I of the problem
+  - functions for the Part II of the problem
+  
+4. a test of function with a set of ``assert``s to check that my computation function seem to give an ok result (using the examples provided with the problem)
+
+5. finally, the main part that solves the problem by running the computation functions on the actual inputs I was given (that depend on the user)
+
+## Day 1: Not Quite Lisp
+
+#### Answers
+**Part I: 74 • Part II: 1795**
+
+Day 1 is pretty straight-forward. The only small ruse I've used is in data preparation: rather than keeping the ``(`` and ``)`` characters, I've replaced each with the corresponding movement (either +1 or -1 floor) so I would just have to basic sums further on.
---
+++ 2015/Python/day1.py
@@ -0,0 +1,78 @@
+### =============================================
+### [ ADVENT OF CODE ] (https://adventofcode.com)
+### 2015 - Mina Pêcheux: Python version
+### ---------------------------------------------
+### Day 1: Not Quite Lisp
+### =============================================
+
+# [ Input parsing functions ]
+# ---------------------------
+def parse_input(data):
+    '''Parses the incoming data into processable inputs.
+    
+    :param data: Provided problem data.
+    :type data: str
+    '''
+    return [ 1 if c == '(' else -1 for c in data.strip() ]
+
+# [ Computation functions ]
+# -------------------------
+### PART I
+def compute_floor(inputs):
+    '''Computes the final floor Santa should reach by processing the given floor
+    moves.
+    
+    :param inputs: List of floor moves.
+    :type inputs: list(int)
+    '''
+    return sum(inputs)
+    
+### PART II
+def find_basement_position(inputs):
+    '''Finds the position in the list of moves that makes Santa reach the
+    basement (floor -1) the first time.
+    
+    :param changes: List of floor moves.
+    :type changes: list(int)
+    '''
+    floor = 0
+    i = 0
+    while floor >= 0:
+        floor += inputs[i]
+        i += 1
+    return i
+    
+# [ Base tests ]
+# --------------
+def make_tests():
+    '''Performs tests on the provided examples to check the result of the
+    computation functions is ok.'''
+    ### PART I
+    assert compute_floor(parse_input('(())')) == 0
+    assert compute_floor(parse_input('()()')) == 0
+    assert compute_floor(parse_input('(((')) == 3
+    assert compute_floor(parse_input('(()(()(')) == 3
+    assert compute_floor(parse_input('))(((((')) == 3
+    assert compute_floor(parse_input('())')) == -1
+    assert compute_floor(parse_input('))(')) == -1
+    assert compute_floor(parse_input(')))')) == -3
+    assert compute_floor(parse_input(')())())')) == -3
+    ### PART II
+    assert find_basement_position(parse_input(')')) == 1
+    assert find_basement_position(parse_input('()())')) == 5
+
+if __name__ == '__main__':
+    # check function results on example cases
+    make_tests()
+    
+    # get input data
+    data_path = '../data/day1.txt'
+    inputs = parse_input(open(data_path, 'r').read())
+    
+    ### PART I
+    solution = compute_floor(inputs)
+    print('PART I: solution = {}'.format(solution))
+    
+    ### PART II
+    solution = find_basement_position(inputs)
+    print('PART II: solution = {}'.format(solution))
